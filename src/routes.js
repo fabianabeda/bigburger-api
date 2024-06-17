ba024a3b3cf0411453f09c02b019b90f46b94e33
Fabiana Beda <82904498+fabianabeda@users.noreply.github.com>
2024-06-17 19:49:34 -0300

    database

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,19 @@
 import { Router} from 'express'
 
+import { v4 } from 'uuid';
+
+import User from './app/models/User';
 
 const routes = new Router()
 
-routes.get('/', (request, response) =>{
-    return response.status(200).json({ message: 'Hello Word'})
+routes.get('/', async (request, response) =>{
+    const user = await User.create({
+        id: v4,
+        name: 'Fabiana2',
+        email: 'fabiana2@email.com',
+        password_hash: 'wiwuejrirdj',
+    });
+    return response.status(201).json(user);
 })
 
 export default routes
