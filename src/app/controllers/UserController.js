465b21836ee4cfc60e4efe092d9bf49fdf3d5abd
Fabiana Beda <82904498+fabianabeda@users.noreply.github.com>
2024-06-21 21:49:50 -0300

    Validacao de e-mail duplicado

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -4,7 +4,7 @@ import * as Yup from 'yup';
 
 class UserController {
     async store(request, response) {
-        const schema = Yup.object({
+        const schema = Yup.object().shape({
             name: Yup.string().required(),
             email: Yup.string().email().required(),
             password_hash: Yup.string().min(6).required(),
@@ -12,13 +12,23 @@ class UserController {
         });
         
         try {
-            schema.validateSync(request.body, { abortEarly: false});
-        } catch(err) {
-            return response.status(400).json({ error: err.errors })
+            await schema.validate(request.body, { abortEarly: false });
+        } catch (err) {
+            return response.status(400).json({ error: err.errors });
         }
 
-
         const { name, email, password_hash, admin } = request.body;
+
+       
+        const userExists = await User.findOne({
+            where: { email },
+        });
+
+        if (userExists) {
+            return response.status(400).json({ error: 'Email already exists' });
+        }
+
+        console.log(userExists);
 
         const user = await User.create({
             id: v4(),
@@ -28,14 +38,16 @@ class UserController {
             admin,
         });
 
-        return response.status(201).json({
-            id: user.id,
-            name, 
-            email,
-            admin,
-        });
-    }
+            return response.status(201).json({
+                id: user.id,
+                name, 
+                email,
+                admin,
+            });
+        } catch (err) {
+            return response.status(500).json({ error: 'Internal server error' });
+        }
+    
 }
 
 export default new UserController();
-
