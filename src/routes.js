43073fb59ab26092eba91e5c93bbd39ae834cac0
Fabiana Beda <82904498+fabianabeda@users.noreply.github.com>
2024-07-19 10:25:01 -0300

    Criação da tabela de categorias

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import ProductController from './app/controllers/ProductController';
+import CategoryController from './app/controllers/CategoryController';
 
 const routes = new Router();
 
@@ -18,6 +19,9 @@ routes.use(authMiddleware);
 
 routes.post('/products', upload.single('file'), ProductController.store);
 routes.get('/products', ProductController.index);
+
+routes.post('/categories', CategoryController.store);
+routes.get('/categories', CategoryController.index);
 
 
 export default routes;
