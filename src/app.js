7219ecd5e4eb53372a8f763b00a5a4d3c782d945
fabianabeda <fabiana.beda@gmail.com>
2025-06-10 21:27:59 -0300

    fix: adiciona1

--- src/app.js
+++ src/app.js
@@ -1,43 +1,41 @@
 import express from 'express';
-import path, { resolve } from 'node:path';
+import { resolve } from 'node:path';
 import { fileURLToPath } from 'url';
 import cors from 'cors';
 import routes from './routes.js';
 import './database/index.js';
 
-// Corrigindo __dirname para ES Modules
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = resolve(__filename, '..');
 
 class App {
-    constructor() {
-        this.app = express();
+  constructor() {
+    this.app = express();
 
-        this.app.use(cors({
-            origin: 'http://localhost:5173',
-        }));
-        this.middlewares();
-        this.routes();
-    }
+    this.app.use(cors({
+      origin: 'http://localhost:5173',
+    }));
+    this.middlewares();
+    this.routes();
+  }
 
-    middlewares() {
-        this.app.use(express.json());
+  middlewares() {
+    this.app.use(express.json());
 
-        this.app.use(
-            '/product-file',
-            express.static(resolve(__dirname, '..', 'uploads'))
-        );
+    this.app.use(
+      '/product-file',
+      express.static(resolve(__dirname, '..', 'uploads'))
+    );
 
-        this.app.use(
-            '/category-file',
-            express.static(resolve(__dirname, '..', 'uploads'))
-        );
-    }
+    this.app.use(
+      '/category-file',
+      express.static(resolve(__dirname, '..', 'uploads'))
+    );
+  }
 
-    routes() {
-        this.app.use(routes);
-    }
+  routes() {
+    this.app.use(routes);
+  }
 }
 
 export default new App().app;
-
--- src/database/index.js
+++ src/database/index.js
@@ -1,35 +1,44 @@
-import Sequelize from "sequelize";
+import { Sequelize } from 'sequelize';
 import mongoose from 'mongoose';
-import configDatabase from '../config/database';
+import configDatabase from '../config/database.js';
 
-import User from "../app/models/User";
-import Product from "../app/models/Product";
-import Category from "../app/models/Category";
+import User from '../app/models/User.js';
+import Product from '../app/models/Product.js';
+import Category from '../app/models/Category.js';
 
 const models = [User, Product, Category];
 
 class Database {
-    constructor(){
-        this.init();
-        this.mongo();
-    }
+  constructor() {
+    this.init();
+    this.mongo();
+  }
 
-    init() {
-        this.connection = new Sequelize(configDatabase);
-        this.connection.authenticate()
-            .then(() => console.log('Conectado ao banco de dados SQL'))
-            .catch(err => console.error('Erro ao conectar ao banco de dados SQL:', err));
+  init() {
+    this.connection = new Sequelize(configDatabase);
 
-        models
-            .map((model) => model.init(this.connection))
-            .map((model) => model.associate && model.associate(this.connection.models));
-    }
+    this.connection.authenticate()
+      .then(() => console.log('Conectado ao banco de dados SQL'))
+      .catch(err => console.error('Erro ao conectar ao banco de dados SQL:', err));
 
-    mongo() {
-        mongoose.connect('mongodb://localhost:27017/bigburger')
-            .then(() => console.log('Conectado ao MongoDB'))
-            .catch(err => console.error('Erro ao conectar ao MongoDB:', err));
-    }
+    models
+      .forEach(model => {
+        model.init(this.connection);
+      });
+
+    models
+      .forEach(model => {
+        if (model.associate) {
+          model.associate(this.connection.models);
+        }
+      });
+  }
+
+  mongo() {
+    mongoose.connect('mongodb://localhost:27017/bigburger')
+      .then(() => console.log('Conectado ao MongoDB'))
+      .catch(err => console.error('Erro ao conectar ao MongoDB:', err));
+  }
 }
 
 export default new Database();
